8b3fe43d7a3d92c6ef5106bf43bb2307079837f3
Emmanuel Obuobi <oboubimakaveli@gmail.com>
2025-08-02 21:41:28 +0000

    UPD

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,19 @@
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin', // Reanimated plugin has to be listed last.
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './',
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        },
+      ],
     ],
   };
 };
