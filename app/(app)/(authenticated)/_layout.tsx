8ecbd243ed50c8f0479bd6b239e68580ddbe1993
Emmanuel Obuobi <oboubimakaveli@gmail.com>
2025-08-23 15:06:32 +0000

    create-chat

--- app/(app)/(authenticated)/_layout.tsx
+++ app/(app)/(authenticated)/_layout.tsx
@@ -1,7 +1,9 @@
 import { useAuth } from '@/providers/AuthProvider';
+import ChatProvider from '@/providers/ChatProvider';
 import { Ionicons } from '@expo/vector-icons';
 import { Redirect, Stack, useRouter } from 'expo-router';
 import { TouchableOpacity } from 'react-native';
+import { Chat } from 'stream-chat-expo';
 
 
 const Layout = () => {
@@ -12,40 +14,42 @@ const Layout = () => {
     return <Redirect href="/login" />;
   }
   return (
-  <Stack>
-    <Stack.Screen 
-      name="(tabs)"
-    options={{
-      headerShown: false,
-      }}
-    />
-    <Stack.Screen 
-      name="(modal)/create-chat"
-      options={{
-      presentation: 'modal',
-      title: 'Create Chat',
-      headerLeft: () => (
-        <TouchableOpacity onPress={() => router.back()}>
-          <Ionicons name="close" size={24} color="black" />
-        </TouchableOpacity>
-      ),
-      }}
-    />
-    <Stack.Screen 
-      name="consultation/schedule"
-      options={{
-      headerBackTitle: 'Back',
-      title: 'Schedule Consultation',
-      }}
-    />
-    <Stack.Screen 
-      name="chat/[id]/index"
-      options={{
-      headerBackTitle: 'Chats',
-      title: 'Manage Chat',
-      }}
-    />
-  </Stack>
+    <ChatProvider>  
+      <Stack>
+        <Stack.Screen 
+          name="(tabs)"
+        options={{
+          headerShown: false,
+          }}
+        />
+        <Stack.Screen 
+          name="(modal)/create-chat"
+          options={{
+          presentation: 'modal',
+          title: 'Create Chat',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close" size={24} color="black" />
+            </TouchableOpacity>
+          ),
+          }}
+        />
+        <Stack.Screen 
+          name="consultation/schedule"
+          options={{
+          headerBackTitle: 'Back',
+          title: 'Schedule Consultation',
+          }}
+        />
+        <Stack.Screen 
+          name="chat/[id]/index"
+          options={{
+          headerBackTitle: 'Chats',
+          title: 'Manage Chat',
+          }}
+        />
+      </Stack>
+    </ChatProvider>
   );
 };
 
