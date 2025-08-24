ea22d3647cfba96323ac7faf61d36df0e4350838
Emmanuel Obuobi <oboubimakaveli@gmail.com>
2025-08-24 01:37:09 +0000

    chat

--- app/(app)/(authenticated)/(tabs)/chats.tsx
+++ app/(app)/(authenticated)/(tabs)/chats.tsx
@@ -1,9 +1,33 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, TouchableOpacity } from 'react-native'
+import { useAuth } from '@/providers/AuthProvider';
+import { useChatContext } from 'stream-chat-expo';
+import { Stack, useRouter, Link } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 const Page = () => {
+  const { isTherapist} = useAuth();
+  const { client } = useChatContext();
+  const router = useRouter();
+
+
   return (
-    <View>
-      <Text>chats</Text>
+    <View className="flex-1">
+      <Stack.Screen
+        options={{
+          headerRight: () => (
+            <>
+            {isTherapist && (
+            <Link href='/(app)/(authenticated)/(modal)/create-chat' asChild>
+              <TouchableOpacity className="mr-4">
+                <Ionicons name="add-circle-outline" size={24} color="black" />
+              </TouchableOpacity>
+            </Link>
+            )}
+            </>
+          ),
+        }}
+      />
+      <Text>Chats</Text>
     </View>
   )
 }
